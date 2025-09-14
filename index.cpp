cf63c063f0c88efa795687cb2406ab7efcb9a980
Your Name <you@example.com>
2025-09-14 23:50:04 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class book{
+	private:
+		string title;
+		string author;
+		float price;
+	public:
+		book(){
+			title="unknown";
+			author="unknown artist";
+			price=0.0;
+		}
+		book(string t,stgring a,float p){
+			title= t;
+			author= a;
+			price=p;
+		}
+		book(const book &s){
+			title=s.title;
+			author=s.author;
+			price=s.price;
+		}
+		void display(){
+			cout <<"title: "<<title<<endl;
+			cout<<"author: "<<author<<endl;
+			cout <<" price: "<<price<<endl;
+		}
+};
+int main(){
+	book  b;
+	book b2("panchatantra","vishnu sharma",550.60);
+	book b3=b2;
+	cout <<"default constructor"<<endl;
+	b.display();
+	cout<<"parameterized constructor"<<endl;
+	b2.display();
+	cout <<"copy constructor"<<endl;
+	b3.display();
+
+	return 0;
+}
